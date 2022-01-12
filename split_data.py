d9579f56fb256604e80e38cb4060bad5b825bd75
Pavel Senin <Pavel.Senin@sanofi.com>
2022-01-12 09:31:18 +0100

    added the check

--- split_data.py
+++ split_data.py
@@ -5,7 +5,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+import numpy as np
 
+path = Path('./split_indices/').expanduser()
+path.mkdir(parents=True, exist_ok=True)
 prefix = "split_part"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -35,9 +38,15 @@ num_clients = 5
 traindata_split = torch.utils.data.random_split(trainset,
                             [int(trainset.data.shape[0] / num_clients) for _ in range(num_clients)])
 
-path = Path('./split_indices/').expanduser()
-path.mkdir(parents=True, exist_ok=True)
-
 for i in range(len(traindata_split)):
     print("==> saving part " + str(i+1) + " out of", len(traindata_split))
     torch.save(traindata_split[i], path/(prefix+str(i)+'.pt'))
+
+print("==> asserting part 0 correctness.")
+check_tensor = torch.load(path/(prefix+str(0)+'.pt'))
+dataset1 = torch.utils.data.dataset.Subset(trainset, check_tensor.indices)
+dataset2 = torch.utils.data.dataset.Subset(trainset, traindata_split[0].indices)
+if all(np.equal(dataset1.indices, dataset2.indices)):
+    print("==> assert OK.")
+else:
+    print("==> assert is NOT OK.")
