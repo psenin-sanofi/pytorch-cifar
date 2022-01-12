ae9e84e19cf049086934743c92a03686735e2604
Pavel Senin <Pavel.Senin@sanofi.com>
2022-01-12 12:40:43 +0100

    adding a stats counter

---
+++ subset_stats.py
@@ -0,0 +1,24 @@
+import torch
+import torchvision
+
+import argparse
+
+parser = argparse.ArgumentParser(description='counting class labels in a subset')
+parser.add_argument('file')
+args = parser.parse_args()
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True)
+
+subset_idx = torch.load(args.file)
+dataset = torch.utils.data.dataset.Subset(trainset, subset_idx.indices)
+
+buckets = [0]*10
+for i in range(len(dataset.indices)):
+    img, label = dataset[i]
+    buckets[label] = buckets[label]+1
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+for i in range(10):
+    print(calsses[i], buckets[i])
