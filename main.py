87c8fbfc3f317bef92a2d44d752b5b495740e9e0
Pavel Senin <Pavel.Senin@sanofi.com>
2022-01-12 10:07:16 +0100

    added the subset load into the main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 
 import torchvision
@@ -13,9 +12,10 @@ import argparse
 
 from models import *
 from utils import progress_bar
-
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--idx', default=None, type=int, help='index number to use')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
@@ -41,8 +41,16 @@ transform_test = transforms.Compose([
 
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = None
+if args.idx:
+    print('==> Loading a subset ', args.idx)
+    path = Path('./split_indices/').expanduser()
+    prefix = "split_part"
+    subset_idx = torch.load(path/(prefix+str(args.idx)+'.pt'))
+    dataset = torch.utils.data.dataset.Subset(trainset, subset_idx.indices)
+    trainloader = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2)
+else:
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
@@ -51,6 +59,9 @@ testloader = torch.utils.data.DataLoader(
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
+
+print('==> training on ', len(trainloader.dataset), 'images')
+print('==> testing on ', len(testloader.dataset), 'images')
 
 # Model
 print('==> Building model..')
