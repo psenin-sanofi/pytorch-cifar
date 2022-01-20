1aca96301987437760e273a703100b03287ea89c
Pavel Senin <Pavel.Senin@sanofi.com>
2022-01-20 09:31:21 +0100

    adding an ip arg parser

--- server_log.py
+++ server_log.py
@@ -32,8 +32,8 @@ if __name__ == "__main__":
     model = LogisticRegression()
     utils.set_initial_params(model)
     strategy = fl.server.strategy.FedAvg(
-        min_available_clients=2,
+        min_available_clients=4,
         eval_fn=get_eval_fn(model),
         on_fit_config_fn=fit_round,
     )
-    fl.server.start_server("0.0.0.0:8080", strategy=strategy, config={"num_rounds": 5})
+    fl.server.start_server("0.0.0.0:8080", strategy=strategy, config={"num_rounds": 250})
